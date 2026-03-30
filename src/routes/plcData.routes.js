612cde1111a13eb90f8589c049dd1f3e6a428a5a
pankaj-deepnapsoftech <https://github.com/pankaj-deepnapsoftech>
2026-03-30 17:16:01 +0530

    working

--- src/routes/plcData.routes.js
+++ src/routes/plcData.routes.js
@@ -11,12 +11,9 @@ import {
   getPlcErrorDistribution,
   getPlcDowntimeByMachine,
   getPlcTimeDistribution,
-<<<<<<< HEAD
   getMachinePerformance,
-=======
   downloadPlcReportExcel,
   downloadPlcReportPdf
->>>>>>> d4dc21e751ca3241b2944557f60e1dada894e953
 } from "../controller/plcData.controller.js";
 
 const router = Router();
@@ -27,12 +24,9 @@ router.get("/download-pdf", downloadMachineStoppagePdf);
 router.get("/analytics/error-distribution", getPlcErrorDistribution);
 router.get("/analytics/downtime-by-machine", getPlcDowntimeByMachine);
 router.get("/analytics/time-distribution", getPlcTimeDistribution);
-<<<<<<< HEAD
 router.get("/machine-performance", getMachinePerformance);
-=======
 router.get("/report/download-pdf",   downloadPlcReportPdf);
 router.get("/report/download-excel", downloadPlcReportExcel);
->>>>>>> d4dc21e751ca3241b2944557f60e1dada894e953
 router.get("/report", getPlcReport);
 router.get("/", getAllPlcData);
 router.get("", getAllPlcData);
